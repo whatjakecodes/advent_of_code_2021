5b33c3a4527a94148a0917ce2bf214e6f0cbebef
Jake Stout <kyjstout@gmail.com>
2021-12-17 09:29:26 -0500

    Complete day 5 part 2

--- lib/day_5.rb
+++ lib/day_5.rb
@@ -9,33 +9,43 @@ def read_input(filename)
     coords = line.split(" -> ")
     point1 = coords[0].split(",").map { |n| n.to_i }
     point2 = coords[1].split(",").map { |n| n.to_i }
-
     x1 = point1[0]
     y1 = point1[1]
-
     x2 = point2[0]
     y2 = point2[1]
 
     grid = expand_grid(grid, x1, y1)
     grid = expand_grid(grid, x2, y2)
 
-    if x1 == x2
-      # is vertical line
-      min_y = [y1,y2].min
-      max_y = [y1,y2].max
-      (min_y..max_y).each do |y|
-        grid[y][x1] += 1
-      end
-    end
+    add_vertical_line(grid, x1, y1, x2, y2)
+    add_horizontal_line(grid, x1, y1, x2, y2)
+  end
 
-    if y1 == y2
-      # is horizontal line
-      min_x = [x1,x2].min
-      max_x = [x1,x2].max
-      (min_x..max_x).each do |x|
-        grid[y1][x] += 1
-      end
-    end
+  grid
+end
+
+def read_input_p2(filename)
+  file_path = File.expand_path(filename, File.dirname(__FILE__) + "/resources")
+
+  grid = [
+    [0, 0],
+    [0, 0]
+  ]
+  File.readlines(file_path).map(&:chomp).each do |line|
+    coords = line.split(" -> ")
+    point1 = coords[0].split(",").map { |n| n.to_i }
+    point2 = coords[1].split(",").map { |n| n.to_i }
+    x1 = point1[0]
+    y1 = point1[1]
+    x2 = point2[0]
+    y2 = point2[1]
+
+    grid = expand_grid(grid, x1, y1)
+    grid = expand_grid(grid, x2, y2)
+
+    add_vertical_line(grid, x1, y1, x2, y2)
+    add_horizontal_line(grid, x1, y1, x2, y2)
+    add_diagonal_line(grid, x1, y1, x2, y2)
   end
 
   grid
@@ -59,6 +69,46 @@ def expand_grid(grid, x, y)
   grid
 end
 
+def add_vertical_line(grid, x1, y1, x2, y2)
+  return if x1 != x2
+  # is vertical line
+  min_y = [y1, y2].min
+  max_y = [y1, y2].max
+  (min_y..max_y).each do |y|
+    grid[y][x1] += 1
+  end
+end
+
+def add_horizontal_line(grid, x1, y1, x2, y2)
+  return if y1 != y2
+
+  min_x = [x1, x2].min
+  max_x = [x1, x2].max
+  (min_x..max_x).each do |x|
+    grid[y1][x] += 1
+  end
+end
+
+def add_diagonal_line(grid, x1, y1, x2, y2)
+  return if y1 == y2 || x1 == x2
+
+  slope = (y2-y1)/(x2-x1)
+  is_45_degree = slope == 1 || slope == -1
+  return unless is_45_degree
+
+  x = x1
+  y = y1
+  height = (y2 - y1).abs + 1
+  height.times.each do |_|
+    grid[y][x] += 1
+
+    x += x2 < x ? -1 : 1
+    y += y2 < y ? -1 : 1
+  end
+
+  grid
+end
+
 def count_intersections(grid)
-  grid.flatten(1).count {|value| value > 1}
+  grid.flatten(1).count { |value| value > 1 }
 end
--- spec/day_5_spec.rb
+++ spec/day_5_spec.rb
@@ -2,92 +2,193 @@
 require 'day_5'
 
 RSpec.describe 'Day 5' do
-  describe "read_input" do
-    it "should read tiny row input" do
-      expected = [
-        [0, 0],
-        [1, 1]
-      ]
-      data = read_input('day5_tiny_row.txt')
-      expect(data.size).to eq(2)
-      expect(data[0].size).to eq(2)
-      expect(data[1].size).to eq(2)
-      expect(data).to eq(expected)
+  context "part 1" do
+    describe "read_input" do
+      it "should read tiny row input" do
+        expected = [
+          [0, 0],
+          [1, 1]
+        ]
+        data = read_input('day5_tiny_row.txt')
+        expect(data.size).to eq(2)
+        expect(data[0].size).to eq(2)
+        expect(data[1].size).to eq(2)
+        expect(data).to eq(expected)
+      end
+
+      it "should read tiny column input" do
+        expected = [
+          [0, 1],
+          [0, 1]
+        ]
+        data = read_input('day5_tiny_column.txt')
+        expect(data.size).to eq(2)
+        expect(data[0].size).to eq(2)
+        expect(data[1].size).to eq(2)
+        expect(data).to eq(expected)
+      end
+
+      it "should read sample input" do
+        expected = [
+          [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+          [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+          [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+          [0, 1, 1, 2, 1, 1, 1, 2, 1, 1],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [2, 2, 2, 1, 1, 1, 0, 0, 0, 0]
+        ]
+        expect(read_input('day5_sample.txt')).to eq(expected)
+      end
     end
 
-    it "should read tiny column input" do
-      expected = [
-        [0, 1],
-        [0, 1]
-      ]
-      data = read_input('day5_tiny_column.txt')
-      expect(data.size).to eq(2)
-      expect(data[0].size).to eq(2)
-      expect(data[1].size).to eq(2)
-      expect(data).to eq(expected)
+    describe "expand_grid" do
+      it 'should resize empty grid' do
+        expect(expand_grid([[0]], 1, 1)).to eq([
+                                                 [0, 0],
+                                                 [0, 0]
+                                               ])
+      end
+
+      it "should resize non-empty grid" do
+        expect(expand_grid([[0, 0], [0, 0]], 3, 3)).to eq([
+                                                            [0, 0, 0, 0],
+                                                            [0, 0, 0, 0],
+                                                            [0, 0, 0, 0],
+                                                            [0, 0, 0, 0]
+                                                          ])
+      end
+    end
+
+    describe "count_intersections" do
+      it "should count no intersections" do
+        expect(count_intersections([[0]])).to eq(0)
+        expect(count_intersections([[1]])).to eq(0)
+        expect(count_intersections([[0, 0], [0, 0]])).to eq(0)
+        expect(count_intersections([[1, 1], [1, 1]])).to eq(0)
+      end
+
+      it "should count overlapping point" do
+        expect(count_intersections([[2]])).to eq(1)
+      end
+
+      it "should count intersections" do
+        expect(count_intersections([[1, 2], [0, 1]])).to eq(1)
+      end
+    end
+
+    describe "answer" do
+      it 'should calculate sample intersections' do
+        grid = read_input('day5_sample.txt')
+        expect(count_intersections(grid)).to eq(5)
+      end
+
+      it 'should calculate my intersections' do
+        grid = read_input('day5.txt')
+        expect(count_intersections(grid)).to eq(5585)
+      end
+    end
+  end
+
+  context "part 2" do
+    describe "add_diagonal_line" do
+      it 'should add small diagonal' do
+        grid = [
+          [0, 0],
+          [0, 0]
+        ]
+        expect(add_diagonal_line(grid, 0, 1, 1, 0)).to eq([
+                                                            [0, 1],
+                                                            [1, 0]
+                                                          ])
+
+        grid = [
+          [0, 0],
+          [0, 0]
+        ]
+        expect(add_diagonal_line(grid, 1, 0, 0, 1)).to eq([
+                                                            [0, 1],
+                                                            [1, 0]
+                                                          ])
+      end
+
+      it 'should add small negative slope diagonal' do
+        grid = [
+          [0, 0],
+          [0, 0]
+        ]
+        expect(add_diagonal_line(grid, 0, 0, 1, 1)).to eq([
+                                                            [1, 0],
+                                                            [0, 1]
+                                                          ])
+        grid = [
+          [0, 0],
+          [0, 0]
+        ]
+        expect(add_diagonal_line(grid, 1, 1, 0, 0)).to eq([
+                                                            [1, 0],
+                                                            [0, 1]
+                                                          ])
+      end
+
+      it 'should add large slope diagonals' do
+        grid = [
+          [0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]
+        ]
+        grid = add_diagonal_line(grid, 0, 0, 1, 1)
+        expect(grid).to eq([
+                             [1, 0, 0],
+                             [0, 1, 0],
+                             [0, 0, 0]
+                           ])
+
+        grid = add_diagonal_line(grid, 0, 2, 2, 0)
+        expect(grid).to eq([
+                             [1, 0, 1],
+                             [0, 2, 0],
+                             [1, 0, 0]
+                           ])
+
+        grid = add_diagonal_line(grid, 2, 2, 1, 1)
+        expect(grid).to eq([
+                             [1, 0, 1],
+                             [0, 3, 0],
+                             [1, 0, 1]
+                           ])
+      end
     end
 
     it "should read sample input" do
       expected = [
-        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-        [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-        [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-        [0, 1, 1, 2, 1, 1, 1, 2, 1, 1],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [2, 2, 2, 1, 1, 1, 0, 0, 0, 0]
+        [1, 0, 1, 0, 0, 0, 0, 1, 1, 0],
+        [0, 1, 1, 1, 0, 0, 0, 2, 0, 0],
+        [0, 0, 2, 0, 1, 0, 1, 1, 1, 0],
+        [0, 0, 0, 1, 0, 2, 0, 2, 0, 0],
+        [0, 1, 1, 2, 3, 1, 3, 2, 1, 1],
+        [0, 0, 0, 1, 0, 2, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+        [2, 2, 2, 1, 1, 1, 0, 0, 0, 0],
       ]
-      expect(read_input('day5_sample.txt')).to eq(expected)
+      expect(read_input_p2('day5_sample.txt')).to eq(expected)
+    end
+    describe "answerP2" do
+      it 'should calculate sample intersections' do
+        grid = read_input_p2('day5_sample.txt')
+        expect(count_intersections(grid)).to eq(12)
+      end
+
+      it 'should calculate my intersections' do
+        grid = read_input_p2('day5.txt')
+        expect(count_intersections(grid)).to eq(17193)
+      end
     end
   end
 
-  describe "expand_grid" do
-    it 'should resize empty grid' do
-      expect(expand_grid([[0]], 1, 1)).to eq([
-                                               [0, 0],
-                                               [0, 0]
-                                             ])
-    end
-
-    it "should resize non-empty grid" do
-      expect(expand_grid([[0, 0], [0, 0]], 3, 3)).to eq([
-                                                          [0, 0, 0, 0],
-                                                          [0, 0, 0, 0],
-                                                          [0, 0, 0, 0],
-                                                          [0, 0, 0, 0]
-                                                        ])
-    end
-  end
-
-  describe "count_intersections" do
-    it "should count no intersections" do
-      expect(count_intersections([[0]])).to eq(0)
-      expect(count_intersections([[1]])).to eq(0)
-      expect(count_intersections([[0,0],[0,0]])).to eq(0)
-      expect(count_intersections([[1,1],[1,1]])).to eq(0)
-    end
-
-    it "should count overlapping point" do
-      expect(count_intersections([[2]])).to eq(1)
-    end
-
-    it "should count intersections" do
-      expect(count_intersections([[1,2],[0,1]])).to eq(1)
-    end
-  end
-
-  describe "answer" do
-    it 'should calculate sample intersections' do
-      grid = read_input('day5_sample.txt')
-      expect(count_intersections(grid)).to eq(5)
-    end
-
-    it 'should calculate my intersections' do
-      grid = read_input('day5.txt')
-      expect(count_intersections(grid)).to eq(5585)
-    end
-  end
 end
