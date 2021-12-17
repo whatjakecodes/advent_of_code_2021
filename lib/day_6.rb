abcfd44cd52faaae88ecf8787f33e47eb6dddb3b
Jake Stout <kyjstout@gmail.com>
2021-12-17 11:20:53 -0500

    Complete day 6 part 2

--- lib/day_6.rb
+++ lib/day_6.rb
@@ -3,6 +3,18 @@ def read_input(filename)
   File.read(file_path).split(",").map(&:to_i)
 end
 
+def read_input_p2(filename)
+  file_path = File.expand_path(filename, File.dirname(__FILE__) + "/resources")
+
+  fish_map = get_empty_fish_map
+
+  File.read(file_path).split(",").each do |fish_timer|
+    fish_map[fish_timer.to_i] += 1
+  end
+
+  fish_map
+end
+
 def get_fish(initial_fish, days)
   fishies = initial_fish.dup
 
@@ -22,4 +34,20 @@ def get_fish(initial_fish, days)
   end
 
   fishies
+end
+
+def get_fish_p2(initial_fish, days)
+  fish_map = initial_fish.dup
+
+  days.times.each do |_|
+    birthing = fish_map.shift
+    fish_map[6] += birthing
+    fish_map.push(birthing)
+  end
+
+  fish_map
+end
+
+def get_empty_fish_map
+  Array.new(9, 0)
 end
--- spec/day_6_spec.rb
+++ spec/day_6_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 require 'day_6'
 
+
+
 RSpec.describe 'Day 6' do
   describe "part 1" do
     it 'should answer sample' do
@@ -18,6 +20,34 @@ RSpec.describe 'Day 6' do
     it 'should handle 1 fish' do
       expect(get_fish([0], 0)).to eq([0])
       expect(get_fish([0], 1)).to eq([6,8])
+    end
+  end
+
+  describe "part 2" do
+    it 'should parse data into hash' do
+      initial_fish = read_input_p2('day6_sample.txt')
+      expected = [0,1,1,2,1,0,0,0,0]
+      expect(initial_fish).to eq(expected)
+    end
+
+    it 'should answer sample' do
+      initial_fish = read_input_p2('day6_sample.txt')
+      expect(get_fish_p2(initial_fish, 18).sum).to eq(26)
+      expect(get_fish_p2(initial_fish, 80).sum).to eq(5934)
+      expect(get_fish_p2(initial_fish, 256).sum).to eq(26984457539)
+    end
+
+    it 'should answer my input' do
+      initial_fish = read_input_p2('day6.txt')
+      expect(get_fish_p2(initial_fish, 256).sum).to eq(1689540415957)
+    end
+
+    it 'should handle 1 fish' do
+      fish_map = get_empty_fish_map
+      fish_map[0] = 1
+
+      expect(get_fish_p2(fish_map, 0)).to eq([1,0,0,0,0,0,0,0,0])
+      expect(get_fish_p2(fish_map, 1)).to eq([0,0,0,0,0,0,1,0,1])
     end
   end
 end
